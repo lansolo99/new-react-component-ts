f0ac005edfb53c945f4993127a48c1280f467afd
lansolo99 <lansolo99@hotmail.fr>
2022-05-16 10:22:20 +0200

    Add props within functional cpt template

--- src/templates/ts/functional.tsx
+++ src/templates/ts/functional.tsx
@@ -1,3 +1,7 @@
-const COMPONENT_NAME = () => <div>Test content</div>;
+type Props = {
+  prop1: "prop1";
+};
+
+const COMPONENT_NAME = ({ prop1 }: Props) => <div>Test prop1: {prop1}</div>;
 
 export default COMPONENT_NAME;
